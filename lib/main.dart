742478e1b5aaa0502c9bf86f81a6036da7309e77
GwinzTV <joshiyin5@gmail.com>
2023-11-09 22:12:26 +0000

    first change

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Gwinz Devx'),
+      home: const MyHomePage(title: 'Flutter Demo Gwinz Dev'),
     );
   }
 }
